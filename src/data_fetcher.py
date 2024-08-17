7b25888f7c50d0167112743e65925eeb09f4cb83
Neel Patel <neelpatel@smu.edu>
2024-08-17 18:15:52 -0500

    add validation to data_fetcher

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -1,11 +1,20 @@
 from config import NEWS_API_KEY
 from newsapi import NewsApiClient
 
-newsapi = NewsApiClient(api_key=NEWS_API_KEY)
+news_api = NewsApiClient(api_key=NEWS_API_KEY)
 
 def fetch_news(stock):
-    all_related_articles = newsapi.get_everything(q=stock, sort_by='publishedAt')
-    return all_related_articles['articles']
+    all_related_articles = news_api.get_everything(q=stock, sort_by='publishedAt')
+    if all_related_articles['status'] == 'ok':
+        if 'articles' in all_related_articles:
+            return all_related_articles['articles']
+        else:
+            print(f'No articles found for {stock}')
+            return []
+    else:
+        print(f'Failed to reach news for {stock}.')
+        return []
+
 
 def fetch_news_for_stocks(stocks):
     stock_news = {}
